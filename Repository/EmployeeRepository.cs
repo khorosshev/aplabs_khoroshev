250cf8477993d7143a66efa4bec2cd7ffb90b581
khorosshev <mhenlightened@yandex.ru>
2022-10-19 22:32:36 +0300

    Search and Sort for Employee

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -25,15 +25,14 @@ namespace Repository
 
 
         public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId,
-    EmployeeParameters employeeParameters, bool trackChanges)
+        EmployeeParameters employeeParameters, bool trackChanges)
         {
-            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId) &&
-           (e.Age
-            >= employeeParameters.MinAge && e.Age <= employeeParameters.MaxAge),
-           trackChanges)
-            .OrderBy(e => e.Name)
+            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId),
+            trackChanges)
+            .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
+            .Search(employeeParameters.SearchTerm)
+            .Sort(employeeParameters.OrderBy)
             .ToListAsync();
-
             return PagedList<Employee>
             .ToPagedList(employees, employeeParameters.PageNumber,
             employeeParameters.PageSize);
